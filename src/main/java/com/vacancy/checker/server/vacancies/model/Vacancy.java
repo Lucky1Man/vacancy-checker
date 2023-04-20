4f673c2608dfcd9cc29afee011400c1afbf2b6c3
Orest Tsiatsiak <oresttsytsyak02@gmail.com>
2023-04-20 14:52:49 +0300

    added configuration for Scheduling pool size [2], also forced usage of builder pattern for model classes by making private constructor

--- src/main/java/com/vacancy/checker/server/vacancies/model/Vacancy.java
+++ src/main/java/com/vacancy/checker/server/vacancies/model/Vacancy.java
@@ -1,9 +1,12 @@
 package com.vacancy.checker.server.vacancies.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(setterPrefix = "with")
 public class Vacancy {
 
